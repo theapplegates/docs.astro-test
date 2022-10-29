f68233f3338a822ffc58f224c7d1bd7461053868
Paul <paul@theapplegates.com>
2022-10-28 23:20:48 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react(),
 	],
-	site: `https://docs.paulapplegate.com/en/introduction`,
+	site: `https://docs.paulapplegate.com`,
 });
